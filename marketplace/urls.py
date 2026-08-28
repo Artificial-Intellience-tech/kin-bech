6de7d03cda9117e21097d7e81d8d707dd500eb40
Nirbhik Thapa <nirbhikthapa3@gmail.com>
2026-08-28 16:21:48 +0545

    Add root URL to open dashboard

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from posts.views import home  # <-- adjust if your view has a different name
+
 urlpatterns = [
+    path("", home, name="home"),  # root URL opens dashboard/home
+
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("posts/", include("posts.urls")),
