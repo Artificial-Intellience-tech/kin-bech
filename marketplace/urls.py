0a0845fb77bbdba6c94bbe63d5a0add741419c53
Nirbhik Thapa <nirbhikthapa3@gmail.com>
2026-08-28 16:25:53 +0545

    Fix root URL to use posts.views.home_view

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from posts.views import home  # <-- adjust if your view has a different name
+from posts.views import home_view
 
 urlpatterns = [
-    path("", home, name="home"),  # root URL opens dashboard/home
+    path("", home_view, name="home"),  # root URL opens dashboard/home
 
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
